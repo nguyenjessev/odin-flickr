786a6577652db26a161478367c49ff241f605c70
Jesse Nguyen <nguyenjessev@gmail.com>
2022-01-15 12:11:23 -0500

    Allow searching for users by NSID and displaying photos

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,13 @@
 class StaticPagesController < ApplicationController
-  def index; end
+  require 'flickr'
+
+  def index
+    @flickr = Flickr.new
+
+    @photos = if params[:flickr_id].blank?
+                @flickr.photos.getRecent
+              else
+                @flickr.photos.search user_id: params[:flickr_id]
+              end
+  end
 end
